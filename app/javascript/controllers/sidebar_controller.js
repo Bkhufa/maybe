b4a5fb3f7a91b255e6e71927248c0ed4fcd14656
Bryan Khufa <47885514+Bkhufa@users.noreply.github.com>
2025-01-31 07:09:07 +0700

    Update sidebar_controller.js

--- app/javascript/controllers/sidebar_controller.js
+++ app/javascript/controllers/sidebar_controller.js
@@ -1,11 +1,10 @@
-// controllers/sidebar_controller.js
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["panel"]
+  static targets = ["panel", "icon"]
 
   toggle() {
     this.panelTarget.classList.toggle("-translate-x-full")
-    this.panelTarget.classList.toggle("md:-translate-x-0")
+    this.iconTarget.classList.toggle("rotate-180")
   }
 }
